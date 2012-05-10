d7387f0e0b67cd9f856c12a70faef2c4b72c0719
Simon Richardson <stickupkid@gmail.com>
2012-05-10 22:20:28 +0100

    Fixing fork

--- src/main/haxe/funk/Fork.hx
+++ src/main/haxe/funk/Fork.hx
@@ -1,21 +1,22 @@
 package funk;
 
 interface IFork<T> {
-	function andThen(branch : Void -> Void) : Void -> Void;
+	function andThen(branch : Void -> Void, ?args : Array<Dynamic>) : Void -> Void;
 	function andContinue(branch : Void -> Void) : T -> Void;
 }
 
 enum Fork<T> {
 	fork(func : (Void -> Void) -> Void);
+	forkN(func : (Void -> Void) -> Array<Dynamic> -> Void, ?args : Array<Dynamic>);
 }
 
 class ForkType {
 	
-	public static function andThen<T>(branch : Fork<T>, func : Void -> Void) : Void -> Void {
-		return new ForkImpl<T>(branch).andThen(func);
+	public static function andThen<T>(branch : Fork<T>, func : Void -> Void, ?args : Array<Dynamic>) : Void -> Void {
+		return new ForkImpl<T>(branch).andThen(func, args);
 	}
 	
-	public static function andContinue<T>(branch : Fork<T>, func : Void -> Void) : Void -> Void {
+	public static function andContinue<T>(branch : Fork<T>, func : Void -> Void) : T -> Void {
 		return new ForkImpl<T>(branch).andContinue(func);
 	}
 }
@@ -28,26 +29,28 @@ class ForkImpl<T> implements IFork<T> {
 		_fork = branch;
 	}
 	
-	public function andThen(f : Void -> Void) : Void -> Void {
+	public function andThen(f : Void -> Void, ?args : Array<Dynamic>) : Void -> Void {
 		var inner = function() : Void {
-			Reflect.callMethod(null, f, []);
+			Reflect.callMethod(null, f, args != null ? args : []);
 		};
 		
 		return function() : Void {
 			switch(_fork) {
 				case fork(func): Reflect.callMethod(null, func, [inner]); 
+				case forkN(func, args): Reflect.callMethod(null, func, args == null ? [inner] : [inner, args]); 
 			}
 		};
 	}
 	
 	public function andContinue(f : Void -> Void) : T -> Void {
 		var inner = function(args : Array<Dynamic>) : Void {
-			Reflect.callMethod(null, f, args);
+			Reflect.callMethod(null, f, args != null ? args : []);
 		};
 		
 		return function(args : T) : Void {
 			switch(_fork) {
-				case fork(func): Reflect.callMethod(null, func, [inner]); 
+				case fork(func): Reflect.callMethod(null, func, [inner]);
+				case forkN(func, args): Reflect.callMethod(null, func, args == null ? [inner] : [inner, args]);
 			}
 		};
 	}
