7598d00269f24e0103af4d655c208b576eda805e
Simon Richardson <stickupkid@gmail.com>
2012-11-01 17:17:44 +0000

    Fixing integration tests

--- src/test/haxe/suites/integration/ListTest.hx
+++ src/test/haxe/suites/integration/ListTest.hx
@@ -1,13 +1,13 @@
 package suites.integration;
 
 import funk.collections.immutable.ListUtil;
-import funk.wildcard.Wildcard;
+import funk.Wildcard;
 
 import massive.munit.Assert;
 import massive.munit.AssertExtensions;
 
 using funk.collections.immutable.ListUtil;
-using funk.wildcard.Wildcard;
+using funk.Wildcard;
 
 using massive.munit.Assert;
 using massive.munit.AssertExtensions;
