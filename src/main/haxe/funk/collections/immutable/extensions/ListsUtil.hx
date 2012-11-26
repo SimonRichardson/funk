ea11fda21a846f80c5a5dacb21ba00dae50d12eb
Simon Richardson <stickupkid@gmail.com>
2012-11-26 20:14:42 +0000

    Fixing the toList and cleaning it up.

--- src/main/haxe/funk/collections/immutable/extensions/ListsUtil.hx
+++ src/main/haxe/funk/collections/immutable/extensions/ListsUtil.hx
@@ -4,57 +4,74 @@ import funk.Funk;
 import funk.collections.extensions.Collections;
 import funk.collections.immutable.List;
 import funk.collections.immutable.extensions.Lists;
+import funk.types.Function0;
 import funk.types.Option;
 
 using funk.collections.immutable.extensions.Lists;
 
 class ListsUtil {
 
-	public static function toList<T1, T2>(any : T1) : List<T2> {
+	public static function fill<T>(amount : Int, func : Function0<T>) : List<T> {
 		var list = Nil;
-		return switch(Type.typeof(any)) {
+		while(--amount > -1) {
+			list = list.prepend(func());
+		}
+		return list.reverse();
+	}
+
+	public static function toList<T1, T2>(any : T1) : List<T2> {
+		var arrayToList = function(array : Array<T2>) {
+			var list = Nil;
+
+			for (item in array) {
+				list = list.append(item);
+			}
+
+			return list;
+		};
+
+		var stringToList = function(string : String) {
+			var list = Nil;
+
+			// TODO (Simon) : Replace with string iterator.
+			var count = 0;
+			var iterator = {
+				hasNext: function() {
+					return count < string.length;
+				},
+				next: function() {
+					return string.substr(count++, 1);
+				}
+			};
+
+			for (item in iterator) {
+				list = list.append(cast item);
+			}
+
+			return list;
+		};
+
+		switch(Type.typeof(any)) {
 			case TEnum(e):
-				cast any;
+				if (e == List) {
+					return cast any;
+				}
 			case TObject:
 				if (Std.is(any, Array)) {
-					var array : Array<T2> = cast any;
-					for (item in array) {
-						list = list.append(item);
-					}
-				} else {
-					list = list.append(cast any);
+					return arrayToList(cast any);
+				} else if (Std.is(any, String)) {
+					return stringToList(cast any);
 				}
-				list;
 			case TClass(c):
 				if (c == Array) {
-					var array : Array<T2> = cast any;
-					for (item in array) {
-						list = list.append(item);
-					}
+					return arrayToList(cast any);
 				} else if (c == String) {
-					var string : String = cast any;
-					// TODO (Simon) : Replace with string iterator.
-					var count = 0;
-					var iterator = {
-						hasNext: function() {
-							return count < string.length;
-						},
-						next: function() {
-							return string.substr(count++, 1);
-						}
-					};
-
-					for (item in iterator) {
-						list = list.append(cast item);
-					}
-				} else {
-					list = list.append(cast any);
+					return stringToList(cast any);
 				}
-				list;
 			default:
-				list = list.append(cast any);
-				list;
 		}
+
+		return Nil.append(cast any);
 	}
 
 }
