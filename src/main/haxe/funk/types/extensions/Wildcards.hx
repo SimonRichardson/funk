97de0e3622fdf71b17495e5537351b40f9fb7ab1
Simon Richardson <stickupkid@gmail.com>
2013-01-02 10:29:08 +0000

    Adding wildcards class back again.

---
+++ src/main/haxe/funk/types/extensions/Wildcards.hx
@@ -0,0 +1,99 @@
+package funk.types.extensions;
+
+import funk.collections.Collection;
+import funk.collections.extensions.CollectionsUtil;
+import funk.collections.immutable.List;
+import funk.collections.immutable.extensions.ListsUtil;
+import funk.types.extensions.Anys;
+import funk.types.Wildcard;
+
+class Wildcards {
+
+    public static function toBoolean<T>(wildcard : Wildcard, x : Null<T>) : Bool {
+        return x == null ? false : !!(cast x);
+    }
+
+    public static function toCollection<T>(wildcard : Wildcard, x : T) : Collection<T> {
+        return CollectionsUtil.toCollection(x);
+    }
+
+    public static function toList<T>(wildcard : Wildcard, x : T) : List<T> {
+        return ListsUtil.toList(x);
+    }
+
+    public static function toLowerCase<T>(wildcard : Wildcard, x : T) : String {
+        return toString(wildcard, x).toLowerCase();
+    }
+
+    public static function toString<T>(wildcard : Wildcard, x : T) : String {
+        return Std.string(x);
+    }
+
+    public static function toUpperCase<T>(wildcard : Wildcard, x : T) : String {
+        return toString(wildcard, x).toUpperCase();
+    }
+
+    public static function plus_<T1 : (Int, Float), T2 : (Int, Float)>(wildcard : Wildcard, a : T1, b : T2) : Float {
+        return cast(a) + cast(b);
+    }
+
+    public static function minus_<T1 : (Int, Float), T2 : (Int, Float)>(wildcard : Wildcard, a : T1, b : T2) : Float {
+        return cast(a) - cast(b);
+    }
+
+    public static function multiply_<T1 : (Int, Float), T2 : (Int, Float)>( wildcard : Wildcard,
+                                                                            a : T1,
+                                                                            b : T2
+                                                                            ) : Float {
+        return cast(a) * cast(b);
+    }
+
+    public static function divide_<T1 : (Int, Float), T2 : (Int, Float)>(wildcard : Wildcard, a : T1, b : T2) : Float {
+        return cast(a) / cast(b);
+    }
+
+    public static function modulo_<T1 : (Int, Float), T2 : (Int, Float)>(wildcard : Wildcard, a : T1, b : T2) : Float {
+        return cast(a) % cast(b);
+    }
+
+    public static function lessThan_<T1 : (Int, Float), T2 : (Int, Float)>(wildcard : Wildcard, a : T1, b : T2) : Bool {
+        return cast(a) < cast(b);
+    }
+
+    public static function lessEqual_<T1 : (Int, Float), T2 : (Int, Float)>(    wildcard : Wildcard,
+                                                                                a : T1,
+                                                                                b : T2
+                                                                                ) : Bool {
+        return cast(a) <= cast(b);
+    }
+
+    public static function greaterThan_<T1 : (Int, Float), T2 : (Int, Float)>(  wildcard : Wildcard,
+                                                                                a : T1,
+                                                                                b : T2
+                                                                                ) : Bool {
+        return cast(a) > cast(b);
+    }
+
+    public static function greaterEqual_<T1 : (Int, Float), T2 : (Int, Float)>( wildcard : Wildcard,
+                                                                                a : T1,
+                                                                                b : T2
+                                                                                ) : Bool {
+        return cast(a) >= cast(b);
+    }
+
+    public static function equal_<T1, T2>(wildcard : Wildcard, a : T1, b : T2) : Bool {
+        return Anys.equals(a, b);
+    }
+
+    public static function notEqual_<T1, T2>(wildcard : Wildcard, a : T1, b : T2) : Bool {
+        return !Anys.equals(a, b);
+    }
+
+    public static function binaryAnd_(wildcard : Wildcard, a : Int, b : Int) : Int {
+        return a & b;
+    }
+
+    public static function binaryXor_(wildcard : Wildcard, a : Int, b : Int) : Int {
+        return a ^ b;
+    }
+}
