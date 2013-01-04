0871ba12c6237468cd68e5e269006251f9331225
Simon Richardson <stickupkid@gmail.com>
2013-01-04 17:54:40 +0000

    Mind drivel for the idea of an actor

---
+++ src/main/haxe/funk/actors/Actor.hx
@@ -0,0 +1,67 @@
+package funk.actors;
+
+enum State {
+	Pending;
+	Running;
+	Failed;
+	Aborted;
+}
+
+enum Message<T> {
+	Message(data : T);
+}
+
+enum ActorAddress {
+	Address(address : String);
+}
+
+class ActorAddresses {
+
+	public static function address(value : ActorAddress) : Option<String> {
+		return Type.getEnumParameters(value)[0].toOption();
+	}
+
+	public static function safe(value : ActorAddress) : ActorAddress {
+		var uid = Math.floor(Math.random() + Date.now().getTime()).toString();
+		return Address(switch(address(value)) {
+			case Some(val): Std.format("$uid@$val");
+			case None: uid;
+		});
+	}
+}
+
+
+typedef ActorAddress = {
+	function address() : Option<String>;
+}
+
+typedef Actor = {>ActorAddress
+
+	function actor() : Actor;
+
+	function send<T1, T2>(data : T1) : Promise<T2>;
+}
+
+class LocalActor<T> {
+
+	public function new() {
+
+	}
+
+	public function address() : Option<String> {
+		return None;
+	}
+
+	public function belongsTo(parent : Actor) : Actor {
+		return parent.attach(this);
+	}
+
+	public function send(message : Message) : Promise<T> {
+
+	}
+}
+
+var l0 = new LocalActor();
+var l1 = new LocalActor();
+
+l0.send("Hello", l1);
