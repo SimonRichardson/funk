ff3b7798bde256f44d4d250ca57caf6608b16efa
Simon Richardson <simon@ustwo.co.uk>
2013-01-17 18:38:52 +0000

    Fixing main example 01

--- examples/main/flash/Example01.hx
+++ examples/main/flash/Example01.hx
@@ -18,27 +18,38 @@ class Example01 extends Sprite {
 	public function new() {
 		super();
 
-		var sprites = ListsUtil.fill(4)(Pass.instanceOf(Sprite));
+		// Create a list of 4 sprite instances.
+		var sprites = ListsUtil.fill(6)(Pass.instanceOf(Sprite));
+		// Map the sprites to their graphics object and apply a
+		// function on each of them.
 		sprites.map(_.graphics()).foreach(function(g) {
-			g.beginFill(0xff00ff);
+			g.beginFill(0x1d1d1d);
 			g.drawRect(0.0, 0.0, 20.0, 20.0);
 			g.endFill();
 		});
 
+		// Combine the Sprite objects with their index, resulting
+		// in a List.&lt;Tuple2.&lt;Sprite, Int&gt;&gt;
+		//
+		// Apply a function on each tuple in order to position the
+		// sprite based on its index in the list.
 		sprites.zipWithIndex().foreach(function(tuple) {
 			var sprite : Sprite = tuple._1();
 			var index : Int = tuple._2();
 
-			var pos = index * 32;
-			
-			sprite.x = 21;
+			var pos = index * 21;
+
+			sprite.x = pos;
+			sprite.y = pos;
 		});
 
+		// Add all sprites to the display list
 		sprites.foreach(_.addChild(this));
 	}
 
 	public static function main() {
-		new Example01();
+		var example = new Example01();
+		flash.Lib.current.addChild(example);
 	}
 }
 
