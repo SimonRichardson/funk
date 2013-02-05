7b285d2a9b45bb43f37b45a3666fe63418628c0f
Simon Richardson <simon@ustwo.co.uk>
2013-02-05 22:10:07 +0000

    Adding a parser.

--- src/main/haxe/funk/selector/Selector.hx
+++ src/main/haxe/funk/selector/Selector.hx
@@ -1,5 +1,6 @@
 package funk.selector;
 
+import funk.Funk;
 import funk.collections.immutable.List;
 import funk.types.Function1;
 import funk.types.Option;
@@ -19,7 +20,8 @@ class Selector {
 	public function query(selector : String) : Void {
 		var lexer = new Lexer(selector);
 		var parser = new Parser(lexer);
-		parser.execute();
+		var list : List<Expr> = parser.execute();
+		trace(list);
 	}
 }
 
@@ -28,6 +30,7 @@ private enum Constant {
 	ClassName(value : String);
 	Ident(value : String);
 	Integer(value : Int);
+	Number(value : Float);
 	Tag(value : String);
 }
 
@@ -40,11 +43,18 @@ private enum Token {
 }
 
 private enum Expr {
-	EVar(name : String, value : Value);
+	EBlock(value : Value);
 }
 
 private enum Value {
-	VIdent(name : String);
+	VAccessor(value : String, next : Value);
+	VClassName(value : String, next : Value);
+	VDesender(next : Value);
+	VInteger(value : Int);
+	VNumber(value : Float);
+	VIdent(value : String, next : Value);
+	VTag(value : String, next : Value);
+	VEnd;
 }
 
 private class Lexer {
@@ -64,20 +74,26 @@ private class Lexer {
 			tuple2(">", function(value){
 				return Gt;
 			}),
+			tuple2("0", function(value) {
+				return Const(Integer(Std.parseInt(value)));
+			}),
+			tuple2("-?[0-9]+\\.[0-9]*", function(value) {
+				return Const(Number(Std.parseFloat(value)));
+			}),
+			tuple2("-?\\.[0-9]+", function(value) {
+				return Const(Number(Std.parseFloat(value)));
+			}),
+			tuple2("-?[1-9][0-9]*", function(value) {
+				return Const(Integer(Std.parseInt(value)));
+			}),
 			tuple2("\\.[a-zA-Z0-9\\-\\_]*", function(value){
 				return Const(ClassName(value));
 			}),
 			tuple2("#[a-zA-Z0-9\\-\\_]*", function(value){
 				return Const(Ident(value));
 			}),
-			tuple2(":[a-zA-Z0-9\\-\\_]*", function(value){
+			tuple2(":[a-zA-Z0-9\\-\\_\\(\\)]*", function(value){
 				return Const(Accessor(value));
-			}),
-			tuple2("0", function(value) {
-				return Const(Integer(Std.parseInt(value)));
-			}),
-			tuple2("-?[1-9][0-9]*", function(value) {
-				return Const(Integer(Std.parseInt(value)));
 			}),
 			tuple2("[a-zA-Z0-9\\-\\_]*", function(value) {
 				return Const(Tag(value));
@@ -128,9 +144,41 @@ private class Parser {
 		_lexer = lexer;
 	}
 
-	public function execute() : Void {
-		for( token in _lexer) {
-			trace(token);
+	public function execute() : List<Expr> {
+		var list = Nil;
+		while(_lexer.hasNext()) {
+			list = list.append(EBlock(matchToken(next())));
+		}
+		return list;
+	}
+
+	private function next() : Option<Token> {
+		return if (_lexer.hasNext()) {
+			Some(_lexer.next());
+		} else {
+			None;
+		}
+	}
+
+	private function matchToken(opt : Option<Token>) : Value {
+		return switch (opt) {
+			case Some(token):
+				switch(token){
+					case Const(const):
+						switch (const) {
+							case Accessor(value): VAccessor(value, matchToken(next()));
+							case ClassName(value): VClassName(value, matchToken(next()));
+							case Ident(value): VIdent(value, matchToken(next()));
+							case Integer(value): VInteger(value);
+							case Number(value): VNumber(value);
+							case Tag(value): VTag(value, matchToken(next()));
+						}
+					case Gt: VDesender(matchToken(next()));
+					case WhiteSpace: matchToken(next());
+					case Eof: VEnd;
+					case Unknown: Funk.error(IllegalOperationError("Unknown token"));
+				}
+			case None: VEnd;
 		}
 	}
 }
