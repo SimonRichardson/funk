768fe7a989284bf9b29027fe2e4daf10c4f4dffc
Simon Richardson <simon@ustwo.co.uk>
2013-03-06 08:52:43 +0000

    Removing more extension files.

--- src/main/haxe/funk/types/Function0.hx
+++ src/main/haxe/funk/types/Function0.hx
@@ -1,3 +1,41 @@
 package funk.types;
 
-typedef Function0<R> = Void -> R;
+using funk.types.Function1;
+using funk.types.Option;
+
+typedef Function0Type<R> = Void -> R;
+
+abstract Function0<R>(Function0Type<R>) from Function0Type<R> to Function0Type<R> {
+
+    inline function new(function : Function0Type<R>) {
+        this = function;
+    }
+}
+
+class Function0Types {
+
+    public static function _0<T1>(func : Function0<T1>) : Function0<T1> {
+        return function() {
+            return func();
+        };
+    }
+
+    public static function map<T1, R>(func : Function0<T1>, mapper : Function1<T1, R>) : Function0<R> {
+        return function() {
+            return mapper(func());
+        };
+    }
+
+    public static function flatMap<T1, R>(func : Function0<T1>, mapper : Function1<T1, Function0<R>>) : Function0<R> {
+        return function() {
+            return mapper(func())();
+        };
+    }
+
+    public static function promote<T1, R>(func : Function0<R>) : Function1<T1, R> {
+        return function(x) {
+            return func();
+        };
+    }
+}
+
--- src/main/haxe/funk/types/Function2.hx
+++ src/main/haxe/funk/types/Function2.hx
@@ -1,3 +1,80 @@
 package funk.types;
 
-typedef Function2<T1, T2, R> = T1 -> T2 -> R;
+using funk.types.Function0;
+using funk.types.Function1;
+using funk.types.Option;
+using funk.types.Tuple2;
+
+typedef Function2Type<T1, T2, R> = T1 -> T2 -> R;
+
+abstract Function2<T1, T2, R>(Function2Type<T1, T2, R>) from Function2Type<T1, T2, R> to Function2Type<T1, T2, R> {
+
+    inline function new(function : Function2Type<T1, T2, R>) {
+        this = function;
+    }
+}
+
+private typedef Curry2<T1, T2, R> = Function1<T1, Function1<T2, R>>;
+
+class Function2Types {
+
+    public static function _1<T1, T2, R>(func : Function2<T1, T2, R>, value1 : T1) : Function1<T2, R> {
+        return function(value2 : T2) {
+            return func(value1, value2);
+        };
+    }
+
+    public static function _2<T1, T2, R>(func : Function2<T1, T2, R>, value2 : T2) : Function1<T1, R> {
+        return function(value1 : T1) {
+            return func(value1, value2);
+        };
+    }
+
+    public static function flip<T1, T2, R>(func : Function2<T1, T2, R>) : Function2<T2, T1, R> {
+        return function (value2, value1) {
+            return func(value1, value2);
+        };
+    }
+
+    public static function compose<T1, T2, C, R>(   from : Function1<C, R>,
+                                                    to : Function2<T1, T2, C>
+                                                    ) : Function2<T1, T2, R> {
+        return function(value0 : T1, value1 : T2) {
+            return from(to(value0, value1));
+        };
+    }
+
+    public static function map<T1, T2, M, R>(   func : Function2<T1, T2, M>,
+                                                mapper : Function1<M, R>
+                                                ) : Function2<T1, T2, R> {
+        return function(value0 : T1, value1 : T2) {
+            return mapper(func(value0, value1));
+        };
+    }
+
+    public static function curry<T1, T2, R>(func : Function2<T1, T2, R>) : Curry2<T1, T2, R> {
+        return function(value0 : T1) {
+            return function(value1 : T2) {
+                return func(value0, value1);
+            };
+        };
+    }
+
+    public static function uncurry<T1, T2, R>(func : Curry2<T1, T2, R>) : Function2<T1, T2, R> {
+        return function(value0 : T1, value1 : T2) {
+            return func(value0)(value1);
+        };
+    }
+
+    public static function untuple<T1, T2, R>(func : Function2<T1, T2, R>) : Function1<Tuple2<T1, T2>, R> {
+        return function(tuple) {
+            return func(tuple._1(), tuple._2());
+        };
+    }
+
+    public static function tuple<T1, T2, R>(func : Function1<Tuple2<T1, T2>, R>) : Function2<T1, T2, R> {
+        return function(value0, value1) {
+            return func(tuple2(value0, value1));
+        };
+    }
+}
