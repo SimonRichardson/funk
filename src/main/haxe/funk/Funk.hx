a626ecc15b359cb767e17e9a25dff3a179063e5c
Simon Richardson <simon@ustwo.co.uk>
2013-04-03 08:45:21 +0100

    Formatting issues.

--- src/main/haxe/funk/Funk.hx
+++ src/main/haxe/funk/Funk.hx
@@ -58,30 +58,18 @@ class Funk {
     @:noUsing
     public static function error<T>(type : Errors, ?posInfo : PosInfos) : T {
         var message = switch(type) {
-            case Abstract:
-                'Type is abstract, you must extend it';
-            case AbstractMethod:
-                'Method is abstract, you must override it';
-            case ActorError(msg):
-                msg;
-            case ArgumentError(msg):
-                msg == null ? 'Arguments supplied are not expected' : msg;
-            case BindingError(msg):
-                msg;
-            case Error(msg):
-                msg;
-            case HttpError(msg):
-                msg;
-            case IllegalOperationError(msg):
-                msg == null ? 'Required operation can not be executed' : msg;
-            case InjectorError(msg):
-                msg;
-            case NoSuchElementError:
-                'No such element exists';
-            case RangeError:
-                'Value is outside of the expected range';
-            case TypeError(msg):
-                msg == null ? 'Type error was thrown' : msg;
+            case Abstract: 'Type is abstract, you must extend it';
+            case AbstractMethod: 'Method is abstract, you must override it';
+            case ActorError(msg): msg;
+            case ArgumentError(msg): msg == null ? 'Arguments supplied are not expected' : msg;
+            case BindingError(msg): msg;
+            case Error(msg): msg;
+            case HttpError(msg): msg;
+            case IllegalOperationError(msg): msg == null ? 'Required operation can not be executed' : msg;
+            case InjectorError(msg): msg;
+            case NoSuchElementError: 'No such element exists';
+            case RangeError: 'Value is outside of the expected range';
+            case TypeError(msg): msg == null ? 'Type error was thrown' : msg;
         }
 
         var stack = CallStack.callStack();
